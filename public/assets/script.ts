74fd069e72a75673befdd100003356eba34bed73
Giovane Richard <giovane@rlvtecnologia.com.br>
2024-02-13 09:58:29 -0300

    feature/create animations css

---
+++ public/assets/script.ts
@@ -0,0 +1,4 @@
+let nextDom = document.querySelector("#next");
+let prevDom = document.querySelector("#prev");
+let carouselDom = document.querySelector(".carousel");
+let listItemDom = document.querySelector(".carousel .list");
