fb8599e8636c5ccf3ed87b3b62be30cc58fb5dfc
Giovane Richard <giovane@rlvtecnologia.com.br>
2024-02-14 08:11:29 -0300

    feature/finalization script and project

--- public/assets/script.ts
+++
@@ -1,4 +0,0 @@
-let nextDom = document.querySelector("#next");
-let prevDom = document.querySelector("#prev");
-let carouselDom = document.querySelector(".carousel");
-let listItemDom = document.querySelector(".carousel .list");
